94482ed8170cdb4f24b3e745de7f5a33fc148568
Pablo Piaggio <Pablo.Piaggio@gmail.com>
2023-12-03 00:46:06 -0600

    Add all files for day 3 both parts

---
+++ python/day03/part1.py
@@ -0,0 +1,53 @@
+STEPS = [
+    (-1, -1),
+    (-1, 0),
+    (-1, 1),
+    (0, -1),
+    (0, 1),
+    (1, -1),
+    (1, 0),
+    (1, 1),
+]
+
+
+def solution(filename: str) -> int:
+    with open(filename, "r") as fp:
+        data: str = fp.read().splitlines()
+
+    total_sum: int = 0
+
+    for row in range(len(data)):
+        line = data[row]
+
+        # check all the line for part numbers
+        index = 0
+        while index < len(line):
+            start = index
+            while index < len(line) and line[index].isnumeric():
+                index += 1
+
+            if line[start].isnumeric() and line[index - 1].isnumeric():
+
+                near_symbol = False
+                for col in range(start, index):
+                    for row_step, col_step in STEPS:
+                        new_row = row + row_step
+                        new_col = col + col_step
+
+                        if 0 <= new_row < len(data) and 0 <= new_col < len(line):
+                            if (not data[new_row][new_col].isnumeric()) and data[
+                                new_row
+                            ][new_col] != ".":
+                                near_symbol = True
+
+                if near_symbol:
+                    total_sum += int(line[start:index])
+
+            index += 1
+
+    return total_sum
+
+
+if __name__ == "__main__":
+    print(solution("./example.txt"))  # 4361
+    print(solution("./input.txt"))  # 519444
---
+++ python/day03/part2.py
@@ -0,0 +1,63 @@
+STEPS = [
+    (-1, -1),
+    (-1, 0),
+    (-1, 1),
+    (0, -1),
+    (0, 1),
+    (1, -1),
+    (1, 0),
+    (1, 1),
+]
+
+
+def get_unique_number_id(data, row, col):
+    start = col
+    while start >= 0 and data[row][start].isnumeric():
+        start -= 1
+    if start < 0:
+        start = 0
+    else:
+        start += 1
+    end = col
+    while end < len(data[row]) and data[row][end].isnumeric():
+        end += 1
+
+    return (row, start, end)
+
+
+def solution(filename: str) -> int:
+    with open(filename, "r") as fp:
+        data: str = fp.read().splitlines()
+
+    total_sum: int = 0
+
+    for row in range(len(data)):
+        line = data[row]
+
+        for index, char in enumerate(line):
+            if line[index] == "*":
+
+                # collect all unique adjacent parts
+                parts = set()
+                for row_step, col_step in STEPS:
+                    new_row = row + row_step
+                    new_col = index + col_step
+
+                    if 0 <= new_row < len(data) and 0 <= new_col < len(line):
+                        if data[new_row][new_col].isnumeric():
+                            part_id = get_unique_number_id(data, new_row, new_col)
+                            parts.add(part_id)
+
+                if len(parts) == 2:
+                    ratio = 1
+                    for (number_row, start, end) in parts:
+                        ratio *= int(data[number_row][start:end])
+
+                    total_sum += ratio
+
+    return total_sum
+
+
+if __name__ == "__main__":
+    print(solution("./example.txt"))  # 467835
+    print(solution("./input.txt"))  # 74528807
