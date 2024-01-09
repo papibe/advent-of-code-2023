2b091d9d0a16d51add7811c3ec86b5a63d766e3b
Pablo Piaggio <Pablo.Piaggio@gmail.com>
2024-01-08 22:38:21 -0600

    Improve annotations and formatting for day 6

--- python/day06/part1_n_2.py
+++ python/day06/part1_n_2.py
@@ -17,8 +17,10 @@ def solution(race_stats: List[Tuple[int, int]]) -> int:
 
 
 if __name__ == "__main__":
-    # print("Part 1 example:", solution([(7, 9), (15, 40), (30, 200)]))  #
-    print("Part 1:", solution([(54, 302), (94, 1476), (65, 1029), (92, 1404)]))  #
+    print("Part 1 example:", solution([(7, 9), (15, 40), (30, 200)]))  # 288
+    print(
+        "Part 1:", solution([(54, 302), (94, 1476), (65, 1029), (92, 1404)])
+    )  # 1195150
 
-    # print("Part 2 example:", solution([(71530, 940200)]))  # 71503
+    print("Part 2 example:", solution([(71530, 940200)]))  # 71503
     print("Part 2:", solution([(54946592, 302147610291404)]))  # 42550411
--- python/day06/part1_n_2_v2.py
+++ python/day06/part1_n_2_v2.py
@@ -6,11 +6,10 @@ def solution(race_stats: List[Tuple[int, int]]) -> int:
     multiplication: int = 1
 
     for (time, distance) in race_stats:
-        # assert (time**2 - 4 * distance) > 0
 
-        sq = math.sqrt(time**2 - 4 * distance)
-        x1 = (time - sq) / 2
-        x2 = (time + sq) / 2
+        sq: float = math.sqrt(time**2 - 4 * distance)
+        x1: float = (time - sq) / 2
+        x2: float = (time + sq) / 2
         if math.modf(x1)[0] == 0:
             x1 += 1
         if math.modf(x2)[0] == 0:
@@ -21,8 +20,10 @@ def solution(race_stats: List[Tuple[int, int]]) -> int:
 
 
 if __name__ == "__main__":
-    # print("Part 1 example:", solution([(7, 9), (15, 40), (30, 200)]))  #
-    print("Part 1:", solution([(54, 302), (94, 1476), (65, 1029), (92, 1404)]))  #
+    print("Part 1 example:", solution([(7, 9), (15, 40), (30, 200)]))  # 288
+    print(
+        "Part 1:", solution([(54, 302), (94, 1476), (65, 1029), (92, 1404)])
+    )  # 1195150
 
-    # print("Part 2 example:", solution([(71530, 940200)]))  # 71503
+    print("Part 2 example:", solution([(71530, 940200)]))  # 71503
     print("Part 2:", solution([(54946592, 302147610291404)]))  # 42550411
