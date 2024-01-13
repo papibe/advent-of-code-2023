460d50d5c3781499909accc7d750df42008787f8
Pablo Piaggio <Pablo.Piaggio@gmail.com>
2024-01-12 21:58:30 -0600

    Improve several things
    - Add and improve annotations
    - Refactor part 2

--- python/day10/part1.py
+++ python/day10/part1.py
@@ -1,8 +1,7 @@
-import re
 from collections import deque
+from typing import Deque, Dict, List, Set, Tuple
 
-
-landscape = {
+landscape: Dict[str, List[Tuple[int, int]]] = {
     "|": [(1, 0), (-1, 0)],
     "-": [(0, -1), (0, 1)],
     "L": [(-1, 0), (0, 1)],
@@ -11,55 +10,52 @@ landscape = {
     "F": [(1, 0), (0, 1)],
     ".": [],
     "S": [(1, 0), (-1, 0)],
-    # "S":[(1, 0), (0, 1)],
 }
 
-START = "S"
+START: str = "S"
 
 
 def solution(filename: str) -> int:
     with open(filename, "r") as fp:
-        data: str = fp.read().splitlines()
+        data: List[str] = fp.read().splitlines()
 
     # find start
     for row, line in enumerate(data):
         for col, char in enumerate(line):
             if char == START:
+                start_row = row
+                start_col = col
                 break
         else:
             continue
         break
-    # print(row, col)
 
-    queue = deque([(row, col, 0)])
-    visited = set([(row, col)])
+    queue: Deque[Tuple[int, int, int]] = deque([(start_row, start_col, 0)])
+    visited: Set[Tuple[int, int]] = set([(start_row, start_col)])
 
     counted = []
     for line in data:
         counted.append([char for char in line])
 
-    max_steps = 0
+    max_steps: int = 0
     while queue:
         row, col, steps = queue.popleft()
         counted[row][col] = str(steps)
-        # for line in counted:
-        #     print(line)
-        # print("-------------------------------------")
         max_steps = max(max_steps, steps)
 
         for (nrow, ncol) in landscape[data[row][col]]:
-            new_row = row + nrow
-            new_col = col + ncol
-            if 0 <= new_row <= len(data) and 0 <= new_col < len(data[0]):
+            new_row: int = row + nrow
+            new_col: int = col + ncol
+            if 0 <= new_row < len(data) and 0 <= new_col < len(data[0]):
 
-                if data[row][col] != "." and (new_row, new_col) not in visited:
+                if data[new_row][new_col] != "." and (new_row, new_col) not in visited:
                     queue.append((new_row, new_col, steps + 1))
                     visited.add((new_row, new_col))
 
-    return max_steps
+    return len(visited) // 2
 
 
 if __name__ == "__main__":
-    # print(solution("./example1.txt"))  #
-    # print(solution("./example2.txt"))  #
-    print(solution("./input.txt"))  #
+    print(solution("./example1.txt"))  # 4
+    print(solution("./example2.txt"))  # 8
+    print(solution("./input.txt"))  # 6701
--- python/day10/part2.py
+++ python/day10/part2.py
@@ -1,213 +1,152 @@
-#
-# Working solution. Will refactor soon
-#
-
-import re
 from collections import deque
+from dataclasses import dataclass
+from typing import Deque, Dict, List, Set, Tuple
 
+Direction = Tuple[int, int]
 
-landscape = {
-    "|": [(1, 0), (-1, 0)],
-    "-": [(0, -1), (0, 1)],
-    "L": [(-1, 0), (0, 1)],
-    "J": [(-1, 0), (0, -1)],
-    "7": [(0, -1), (1, 0)],
-    "F": [(1, 0), (0, 1)],
+LEFT: Direction = (0, -1)
+RIGHT: Direction = (0, 1)
+UP: Direction = (-1, 0)
+DOWN: Direction = (1, 0)
+
+landscape: Dict[str, List[Direction]] = {
+    "|": [UP, DOWN],
+    "-": [LEFT, RIGHT],
+    "L": [UP, RIGHT],
+    "J": [UP, LEFT],
+    "7": [LEFT, DOWN],
+    "F": [RIGHT, DOWN],
     ".": [],
-    "S": [],
 }
 
-START = "S"
-SPACE = "."
+START: str = "S"
+SPACE: str = "."
 
-STEPS = [
-    (-1, -1),
-    (-1, 0),
-    (-1, 1),
-    (0, -1),
-    (0, 1),
-    (1, -1),
-    (1, 0),
-    (1, 1),
+STEPS: List[Direction] = [
+    LEFT,
+    RIGHT,
+    UP,
+    DOWN,
 ]
 
-GOOD_CORNER = {
-    (-1, 0): "-",  # up
-    (1, 0): "-",  # down
-    (0, -1): "|",  # left
-    (0, 1): "|",  # right
-    (-1, -1): "F",  # nw
-    (-1, 1): "7",  # ne
-    (1, -1): "L",  # nw
-    (1, 1): "J",  # ne
-}
+
+@dataclass
+class Cell:
+    value: str
+    is_original: bool = False
+    in_pipe: bool = False
 
 
-def solution(filename: str, s_form: str) -> int:
+def parse(filename: str) -> List[List[str]]:
     with open(filename, "r") as fp:
-        raw_data: str = fp.read().splitlines()
+        raw_data: List[str] = fp.read().splitlines()
 
-    mdata = []
+    grid: List[List[str]] = []
     for line in raw_data:
-        mdata.append([char for char in line])
+        grid.append([char for char in line])
 
-    # patch S
-    # find start
-    for row, line in enumerate(mdata):
+    return grid
+
+
+def get_start_coords(grid: List[List[str]]) -> Tuple[int, int]:
+    for row, line in enumerate(grid):
         for col, char in enumerate(line):
             if char == START:
-                start_row = 2* row
-                start_col = 2* col
-                mdata[row][col] = s_form
-        else:
-            continue
-        break
-
-    # print(len(mdata), len(mdata[0]))
-
-    data = []
-    for _ in range(2*len(mdata)):
-        data.append([{"value": ".", "original": False} for _ in range(2 * len(mdata[0]))])
-    # data = [[{"value": ".", "original": False}] * (2 * len(mdata[0])) for _ in range(2*len(mdata))]
-
-    for row, line in enumerate(mdata):
-        for col, char in enumerate(line):
-            data[2 * row][2 * col] = {"value": char, "original": True}
-
-    # for row in data:
-    #     for item in row:
-    #         print(item["value"], end="")
-    #     print()
-    # print("================================")
+                return (row, col)
+    return (-1, -1)
 
 
-    for row, line in enumerate(data):
+def build_double_grid(original_grid: List[List[str]]) -> List[List[Cell]]:
+    grid: List[List[Cell]] = []
+    for _ in range(2 * len(original_grid)):
+        grid.append([Cell(value=SPACE) for _ in range(2 * len(original_grid[0]))])
+
+    for row, line_ in enumerate(original_grid):
+        for col, char in enumerate(line_):
+            grid[2 * row][2 * col].value = char
+            grid[2 * row][2 * col].is_original = True
+
+    for row, line in enumerate(grid):
         for col, item in enumerate(line):
-            if not item["original"]:
-                # data[row][col]["value"] = "*"
-                # print(row, col)
-                # is connected
+            if item.is_original or item.value != SPACE:
+                continue
 
-                if 0 <= col - 1 and col + 1 < len(data[0]):
-                    if (
-                        (data[row][col - 1]["value"] == "-" and data[row][col + 1]["value"] == "-")
-                        or (data[row][col - 1]["value"] == "-" and data[row][col + 1]["value"] == "7")
-                        or (data[row][col - 1]["value"] == "-" and data[row][col + 1]["value"] == "J")
-                        or (data[row][col - 1]["value"] == "F" and data[row][col + 1]["value"] == "-")
-                        or (data[row][col - 1]["value"] == "F" and data[row][col + 1]["value"] == "7")
-                        or (data[row][col - 1]["value"] == "F" and data[row][col + 1]["value"] == "J")
-                        or (data[row][col - 1]["value"] == "L" and data[row][col + 1]["value"] == "-")
-                        or (data[row][col - 1]["value"] == "L" and data[row][col + 1]["value"] == "7")
-                        or (data[row][col - 1]["value"] == "L" and data[row][col + 1]["value"] == "J")
-                        ):
-                        # print("\tchanged")
-                        data[row][col]["value"] = "-"
-                        # print(row, col)
+            if 0 <= col - 1 and col + 1 < len(grid[0]):
+                left_neighbor = grid[row][col - 1].value
+                right_neighbor = grid[row][col + 1].value
+                if (
+                    RIGHT in landscape[left_neighbor]
+                    and LEFT in landscape[right_neighbor]
+                ):
+                    grid[row][col].value = "-"
 
-                if 0 <= row - 1 and row + 1 < len(data):
-                    if (
-                        (data[row - 1][col]["value"] == "F" and data[row + 1][col]["value"] == "|")
-                        or (data[row - 1][col]["value"] == "F" and data[row + 1][col]["value"] == "L")
-                        or (data[row - 1][col]["value"] == "F" and data[row + 1][col]["value"] == "J")
-                        or (data[row - 1][col]["value"] == "|" and data[row + 1][col]["value"] == "|")
-                        or (data[row - 1][col]["value"] == "|" and data[row + 1][col]["value"] == "L")
-                        or (data[row - 1][col]["value"] == "|" and data[row + 1][col]["value"] == "J")
-                        or (data[row - 1][col]["value"] == "7" and data[row + 1][col]["value"] == "|")
-                        or (data[row - 1][col]["value"] == "7" and data[row + 1][col]["value"] == "J")
-                        or (data[row - 1][col]["value"] == "7" and data[row + 1][col]["value"] == "L")
+            if 0 <= row - 1 and row + 1 < len(grid):
+                up_neighbor = grid[row - 1][col].value
+                down_neighbor = grid[row + 1][col].value
+                if DOWN in landscape[up_neighbor] and UP in landscape[down_neighbor]:
+                    grid[row][col].value = "|"
 
-                        ):
-                        # print("\tchanged")
-                        data[row][col]["value"] = "|"
-                        # print(row, col)
+    return grid
 
 
-        # else:
-        #     continue
-        # break
-
-    
-
-    for row in data:
-        for item in row:
-            print(item["value"], end="")
-        print()
-    print("================================")
-
-
-    #######################################################
-    # marking inside the loop
-    queue = deque([(start_row, start_col, 0)])
-    visited = set([(start_row, start_col)])
-
-    print(start_row, start_col)
+def solve(start_row: int, start_col: int, grid: List[List[Cell]]) -> int:
+    # marking elements that are part of the loop
+    queue: Deque[Tuple[int, int, int]] = deque([(start_row, start_col, 0)])
+    visited: Set[Tuple[int, int]] = set([(start_row, start_col)])
 
     while queue:
         row, col, steps = queue.popleft()
-        data[row][col]["pipe"] = True
+        grid[row][col].in_pipe = True
 
-        for (nrow, ncol) in landscape[data[row][col]["value"]]:
-            new_row = row + nrow
-            new_col = col + ncol
-            if 0 <= new_row <= len(data) and 0 <= new_col < len(data[0]):
+        for (nrow, ncol) in landscape[grid[row][col].value]:
+            new_row: int = row + nrow
+            new_col: int = col + ncol
+            if 0 <= new_row < len(grid) and 0 <= new_col < len(grid[0]):
 
-                if data[row][col]["value"] != "." and (new_row, new_col) not in visited:
+                if grid[row][col].value != SPACE and (new_row, new_col) not in visited:
                     queue.append((new_row, new_col, steps + 1))
                     visited.add((new_row, new_col))
 
-    #######################################################
-
-
-    for row, line in enumerate(data):
+    for row, line in enumerate(grid):
         for col, item in enumerate(line):
-            if "pipe" not in item:
-                data[row][col]["value"] = "."
+            if not item.in_pipe:
+                grid[row][col].value = SPACE
 
-
-    for row in data:
-        for item in row:
-            print(item["value"], end="")
-        print()
-    print("================================")
-
-
+    # count contained cells
+    counter: int = 0
     visited = set()
-    counter = 0
+    outside: Set[Tuple[int, int]] = set()
 
-    outside = set()
+    for row, line in enumerate(grid):
+        for col, cell in enumerate(line):
+            if (row, col) not in visited and cell.value == SPACE:
+                bfs_row: int = row
+                bfs_col: int = col
 
-    print(visited)
+                queue_: Deque[Tuple[int, int]] = deque([(bfs_row, bfs_col)])
+                local_visited: Set[Tuple[int, int]] = set([(bfs_row, bfs_col)])
+                contained: bool = True
 
-    for row, line in enumerate(data):
-        for col, char in enumerate(line):
-            if (row, col) not in visited and data[row][col]["value"] == SPACE:
-                bfs_row = row
-                bfs_col = col
-                print(f"{bfs_row = }, {bfs_col}")
-                queue = deque([(bfs_row, bfs_col)])
-                local_visited = set([(bfs_row, bfs_col)])
-                contained = True
-                while queue:
-                    bfs_row, bfs_col = queue.popleft()
+                while queue_:
+                    bfs_row, bfs_col = queue_.popleft()
                     for row_step, col_step in STEPS:
                         new_row = bfs_row + row_step
                         new_col = bfs_col + col_step
                         if (new_row, new_col) in outside:
                             contained = False
-                        if 0 <= new_row < len(data) and 0 <= new_col < len(data[0]):
+                        if 0 <= new_row < len(grid) and 0 <= new_col < len(grid[0]):
                             if (
-                                data[new_row][new_col]["value"] == SPACE
+                                grid[new_row][new_col].value == SPACE
                                 and (new_row, new_col) not in local_visited
                             ):
-                                queue.append((new_row, new_col))
+                                queue_.append((new_row, new_col))
                                 local_visited.add((new_row, new_col))
                         else:
                             contained = False
+
                 if contained:
-                    # counter += len(local_visited)
-                    print(local_visited, len(local_visited))
                     for r, c in local_visited:
-                        if data[r][c]["original"]:
+                        if grid[r][c].is_original:
                             counter += 1
                 else:
                     outside.update(local_visited)
@@ -216,10 +155,32 @@ def solution(filename: str, s_form: str) -> int:
     return counter
 
 
+def solution(filename: str, start_form: str) -> int:
+    # parse file
+    original_grid: List[List[str]] = parse(filename)
+
+    # get start position
+    start_row, start_col = get_start_coords(original_grid)
+
+    # patch start position with its proper shape
+    original_grid[start_row][start_col] = start_form
+
+    # new position for double grid:
+    start_row *= 2
+    start_col *= 2
+
+    # build double sized grid
+    grid: List[List[Cell]] = build_double_grid(original_grid)
+
+    return solve(start_row, start_col, grid)
+
+
 if __name__ == "__main__":
-    # print(solution("./example0.txt", "F"))  #
-    # print(solution("./example3.txt", "F"))  #
-    # print(solution("./example4.txt", "F")) #
-    # print(solution("./example5.txt", "F")) #
-    # print(solution("./example6.txt", "7")) #
-    print(solution("./input.txt", "|"))  #
+    print(solution("./example0.txt", "F"))  # 0
+    print(solution("./example1.txt", "F"))  # 1
+    print(solution("./example2.txt", "F"))  # 1
+    print(solution("./example3.txt", "F"))  # 4
+    print(solution("./example4.txt", "F"))  # 4
+    print(solution("./example5.txt", "F"))  # 8
+    print(solution("./example6.txt", "7"))  # 10
+    print(solution("./input.txt", "|"))  # 303
