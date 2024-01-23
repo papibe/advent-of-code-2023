5104ab99ccc7cf8740a86508af6824137b0d1ffb
Pablo Piaggio <Pablo.Piaggio@gmail.com>
2024-01-22 22:07:39 -0600

    Improve formatting and annotations for day 17

--- python/day17/part1.py
+++ python/day17/part1.py
@@ -1,15 +1,19 @@
 import heapq as hq
+from typing import List, Set, Tuple
+
+HeapItem = Tuple[int, int, int, int, int, int]
+VisitedItem = Tuple[int, int, int, int, int]
 
 
 def solution(filename: str) -> int:
     with open(filename, "r") as fp:
-        city_map: str = fp.read().splitlines()
+        city_map: List[str] = fp.read().splitlines()
 
     # heat, row, col, dir_row, dir_col, strait steps
-    queue = [(0, 0, 0, 0, 0, 0)]
+    queue: List[HeapItem] = [(0, 0, 0, 0, 0, 0)]
     hq.heapify(queue)
-    visited = set((0, 0, 0, 0, 0))
-    destination = (len(city_map) - 1, len(city_map[0]) - 1)
+    visited: Set[VisitedItem] = set([(0, 0, 0, 0, 0)])
+    destination: Tuple[int, int] = (len(city_map) - 1, len(city_map[0]) - 1)
 
     while queue:
         heat, row, col, dir_row, dir_col, s_steps = hq.heappop(queue)
@@ -18,11 +22,11 @@ def solution(filename: str) -> int:
             return heat
 
         # RIGHT, LEFT, UP, DOWN
-        steps = [(0, 1), (0, -1), (-1, 0), (1, 0)]
+        steps: List[Tuple[int, int]] = [(0, 1), (0, -1), (-1, 0), (1, 0)]
 
         for step_row, step_col in steps:
-            new_row = row + step_row
-            new_col = col + step_col
+            new_row: int = row + step_row
+            new_col: int = col + step_col
 
             if 0 <= new_row < len(city_map) and 0 <= new_col < len(city_map[0]):
                 # can't go back on opposite direction
@@ -33,6 +37,7 @@ def solution(filename: str) -> int:
                 if (step_row, step_col) == (dir_row, dir_col) and s_steps >= 3:
                     continue
 
+                new_steps: int
                 # Same direction: increase steps, if not reset steps
                 if (step_row, step_col) == (dir_row, dir_col):
                     new_steps = s_steps + 1
@@ -42,7 +47,7 @@ def solution(filename: str) -> int:
                 if (new_row, new_col, step_row, step_col, new_steps) in visited:
                     continue
 
-                new_heat = heat + int(city_map[new_row][new_col])
+                new_heat: int = heat + int(city_map[new_row][new_col])
                 hq.heappush(
                     queue,
                     (new_heat, new_row, new_col, step_row, step_col, new_steps),
