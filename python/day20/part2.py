8ea4b05b1abb94afefcc1a44554bdccbbe6f8f8b
Pablo Piaggio <Pablo.Piaggio@gmail.com>
2024-01-25 22:24:01 -0600

    Improve annotations and formatting for day 20

--- python/day20/part2.py
+++ python/day20/part2.py
@@ -1,43 +1,54 @@
 import re
 from collections import deque
 from math import lcm
+from typing import Deque, Dict, List, Match, Optional, Tuple
 
-OFF = 0
-ON = 1
-LOW_PULSE = 2
-HIGH_PULSE = 3
+OFF: int = 0
+ON: int = 1
+LOW_PULSE: int = 2
+HIGH_PULSE: int = 3
+
+Queue = Deque[Tuple[str, int, str]]
 
 
 class Module:
-    def update(self, modules):
+    def __init__(self) -> None:
+        self.name: str = ""
+        self.node_names: List[str] = []
+        self.nodes: List[Module] = []
+
+    def update(self, modules: "Modules") -> None:
         nodes = []
         for node_name in self.node_names:
             if node_name in modules:
                 nodes.append(modules[node_name])
         self.nodes = nodes
 
-    def set_memory(self, modules):
+    def set_memory(self, modules: "Modules") -> None:
         pass
 
-    def receive_high_pulse(self, node_from):
+    def receive_high_pulse(self, node_from: str) -> None:
         pass
 
-    def receive_low_pulse(self, node_from):
+    def receive_low_pulse(self, node_from: str) -> None:
         pass
+
+
+Modules = Dict[str, Module]
 
 
 class Broadcaster(Module):
-    def __init__(self, nodes, queue):
-        self.name = "broadcaster"
-        self.node_names = nodes
-        self.low_pulses = 0
-        self.high_pulses = 0
-        self.queue = queue
+    def __init__(self, nodes: List[str], queue: Queue) -> None:
+        self.name: str = "broadcaster"
+        self.node_names: List[str] = nodes
+        self.low_pulses: int = 0
+        self.high_pulses: int = 0
+        self.queue: Queue = queue
 
-    def receive_low_pulse(self, node_from):
+    def receive_low_pulse(self, node_from: str) -> None:
         self.broadcast()
 
-    def broadcast(self):
+    def broadcast(self) -> None:
         for node in self.nodes:
             self.low_pulses += 1
             # print(self.name, "-low->", node.name)
@@ -45,42 +56,43 @@ class Broadcaster(Module):
 
 
 class Flipflop(Module):
-    def __init__(self, name, nodes, queue):
-        self.name = name
-        self.node_names = nodes
-        self.state = OFF
-        self.low_pulses = 0
-        self.high_pulses = 0
-        self.queue = queue
+    def __init__(self, name: str, nodes: List[str], queue: Queue) -> None:
+        self.name: str = name
+        self.node_names: List[str] = nodes
+        self.state: int = OFF
+        self.low_pulses: int = 0
+        self.high_pulses: int = 0
+        self.queue: Queue = queue
 
-    def receive_low_pulse(self, node_from):
+    def receive_low_pulse(self, node_from: str) -> None:
         self.state = ON if self.state == OFF else OFF
+        sending_pulse: int
         if self.state == ON:
             sending_pulse = HIGH_PULSE
-            msg = "-high-"
+            # msg: str = "-high-"
         else:
             sending_pulse = LOW_PULSE
-            msg = "-low-"
+            # msg: str = "-low-"
 
         for node in self.nodes:
             self.high_pulses += 1
             # print(self.name, msg, node.name)
             self.queue.append((self.name, sending_pulse, node.name))
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return f"t:f {self.name}"
 
 
 class Conjunction(Module):
-    def __init__(self, name, nodes, queue):
-        self.name = name
-        self.node_names = nodes
-        self.low_pulses = 0
-        self.high_pulses = 0
-        self.last_pulse = {}
-        self.queue = queue
+    def __init__(self, name: str, nodes: List[str], queue: Queue) -> None:
+        self.name: str = name
+        self.node_names: List[str] = nodes
+        self.low_pulses: int = 0
+        self.high_pulses: int = 0
+        self.last_pulse: Dict[str, int] = {}
+        self.queue: Queue = queue
 
-    def set_memory(self, modules):
+    def set_memory(self, modules: Modules) -> None:
         # pass
         for node_name, node in modules.items():
             for node_to_name in node.node_names:
@@ -88,98 +100,106 @@ class Conjunction(Module):
                     self.last_pulse[node_name] = LOW_PULSE
                     break
 
-    def send_pulse(self):
+    def send_pulse(self) -> None:
         sending_pulse = LOW_PULSE
-        msg = "-low->"
+        # msg: str = "-low->"
         for node_name, last_pulse in self.last_pulse.items():
             if last_pulse == LOW_PULSE:
                 sending_pulse = HIGH_PULSE
-                msg = "-high->"
+                # msg: str = "-high->"
                 break
         for node in self.nodes:
             # print(self.name, msg, node.name)
             self.queue.append((self.name, sending_pulse, node.name))
 
-    def receive_high_pulse(self, node_from):
+    def receive_high_pulse(self, node_from: str) -> None:
         self.last_pulse[node_from] = HIGH_PULSE
         self.send_pulse()
 
-    def receive_low_pulse(self, node_from):
+    def receive_low_pulse(self, node_from: str) -> None:
         self.last_pulse[node_from] = LOW_PULSE
         self.send_pulse()
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return f"t:c {self.name}, {self.node_names}"
 
 
 class Button:
-    def __init__(self, broadcaster, queue):
-        self.name = "button"
-        self.broadcaster = broadcaster
-        self.low_pulses = 0
-        self.high_pulses = 0
-        self.queue = queue
+    def __init__(self, broadcaster: Broadcaster, queue: Queue) -> None:
+        self.name: str = "button"
+        self.broadcaster: Broadcaster = broadcaster
+        self.low_pulses: int = 0
+        self.high_pulses: int = 0
+        self.queue: Queue = queue
 
-    def press(self):
+    def press(self) -> None:
         self.low_pulses += 1
         # print(self.name, "-low->", "broadcaster")
         self.queue.append((self.name, LOW_PULSE, "broadcaster"))
 
 
 class Orphan(Module):
-    def __init__(self, name):
-        self.name = name
-        self.node_names = []
-        self.nodes = []
+    def __init__(self, name: str) -> None:
+        self.name: str = name
+        self.node_names: List[str] = []
+        self.nodes: List[Module] = []
 
-    def update(self, modules):
+    def update(self, modules: Modules) -> None:
         pass
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return f"t:o {self.name}, {self.node_names}"
 
 
-def parse(filename: str):
+def parse(filename: str) -> Tuple[Button, Modules, Queue, str]:
     with open(filename, "r") as fp:
-        data: str = fp.read().splitlines()
+        data: List[str] = fp.read().splitlines()
 
-    regex = r"^.(\w+) -> (.+)"
-    b_regex = r"broadcaster -> (.+)"
+    regex: str = r"^.(\w+) -> (.+)"
+    b_regex: str = r"broadcaster -> (.+)"
 
-    queue = deque()
+    queue: Queue = deque()
 
-    modules = {}
+    modules: Modules = {}
     for line in data:
+        name: str
+        nodes: List[str]
         if line.startswith("%"):
-            match = re.search(regex, line)
-            name = match.group(1)
-            nodes = match.group(2).split(", ")
-            ff = Flipflop(name, nodes, queue)
+            matches: Optional[Match[str]] = re.search(regex, line)
+
+            assert matches is not None
+            name = matches.group(1)
+            nodes = matches.group(2).split(", ")
+            ff: Flipflop = Flipflop(name, nodes, queue)
             modules[name] = ff
 
         elif line.startswith("&"):
-            match = re.search(regex, line)
-            name = match.group(1)
-            nodes = match.group(2).split(", ")
-            cc = Conjunction(name, nodes, queue)
+            matches = re.search(regex, line)
+
+            assert matches is not None
+            name = matches.group(1)
+            nodes = matches.group(2).split(", ")
+            cc: Conjunction = Conjunction(name, nodes, queue)
             modules[name] = cc
 
         else:  # broadcaster
-            match = re.search(b_regex, line)
+            matches = re.search(b_regex, line)
+
+            assert matches is not None
             # name = match.group(1)
-            nodes = match.group(1).split(", ")
-            broadcaster = Broadcaster(nodes, queue)
+            nodes = matches.group(1).split(", ")
+            broadcaster: Broadcaster = Broadcaster(nodes, queue)
             modules["broadcaster"] = broadcaster
 
     button = Button(broadcaster, queue)
 
     # look for orphan node (exit node)
-    orphan_parent = None
+    orphan_parent: str = ""
     for module_name, module in modules.items():
         for destination in module.node_names:
             if destination not in modules:
                 orphan_parent = module.name
-                orphan2 = Orphan(destination)
+                orphan2: Orphan = Orphan(destination)
                 modules[destination] = orphan2
                 # print(f"-{destination}-")
                 break
@@ -198,10 +218,9 @@ def parse(filename: str):
 
 def solution(filename: str) -> int:
     button, modules, queue, parent_name = parse(filename)
+    parent_module: Conjunction = modules[parent_name]  # type: ignore
 
-    counters = {
-        module_name: 0 for module_name in modules[parent_name].last_pulse.keys()
-    }
+    counters = {module_name: 0 for module_name in parent_module.last_pulse.keys()}
 
     for run in range(1, 5000):
 
@@ -227,7 +246,7 @@ def solution(filename: str) -> int:
             continue
         break
 
-    total_lcm = 1
+    total_lcm: int = 1
     for counter in counters.values():
         total_lcm = lcm(total_lcm, counter)
 
