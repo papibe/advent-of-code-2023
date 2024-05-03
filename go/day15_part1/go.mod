83fc2c1f788238abb913847873d5fb51b9e5bfe9
Pablo Piaggio <Pablo.Piaggio@gmail.com>
2024-05-02 21:07:21 -0500

    Add go solutions and files for day 15 part 1

---
+++ go/day15_part1/go.mod
@@ -0,0 +1,3 @@
+module day15_part1
+
+go 1.21.1
---
+++ go/day15_part1/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+func solution(filename string) int {
+	data, err := os.ReadFile(filename)
+
+	if err != nil {
+		panic("file error")
+	}
+	content := strings.Trim(string(data), "\n")
+	lines := strings.Split(content, "\n")
+	line := lines[0]
+
+	total_sum := 0
+	for _, sequence := range strings.Split(line, ",") {
+		ascii_value := 0
+		for _, char := range sequence {
+			ascii_value += int(char)
+			ascii_value *= 17
+			ascii_value %= 256
+		}
+		total_sum += ascii_value
+	}
+	return total_sum
+}
+
+func main() {
+	fmt.Println(solution("./example.txt")) // 9734203
+	fmt.Println(solution("./input.txt"))   // 568914596391
+}
